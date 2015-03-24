d79891f5526ff502c8802b53d039d038b7f4d90b
龙仕云 <mrlong.com@gmail.com>
2015-03-24 22:03:44 +0800

    修改：    1.增加例子
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

---
+++ hello.cc
@@ -0,0 +1,21 @@
+///usr/local/include/node 内有引用的文件
+// hello.cc
+#include <node.h>
+
+using namespace v8;
+
+void Method(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world444499"));
+}
+
+void init(Handle<Object> exports) {
+  NODE_SET_METHOD(exports, "hello", Method);
+}
+
+NODE_MODULE(addon, init)
+  
+  
+
+  
