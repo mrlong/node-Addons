b5faaacdbf244daf1b32778ce1266a6eef8f2d10
龙仕云 <mrlong.com@gmail.com>
2015-03-24 22:23:14 +0800

    增加方法。回调的功能
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

--- hello.cc
+++ hello.cc
@@ -10,8 +10,62 @@ void Method(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world444499222"));
 }
 
+//两个数相加，有参数传进来了
+void Add(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+
+  if (args.Length() < 2) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+
+  double value = args[0]->NumberValue() + args[1]->NumberValue();
+  Local<Number> num = Number::New(isolate, value);
+
+  args.GetReturnValue().Set(num);
+};
+
+
+//有回调功能
+void RunCallback(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  
+  if (args.Length() < 2) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
+  
+  if (!args[0]->IsString() || !args[1]->IsFunction()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  
+  //Local<String> str = Local<String>::Cast(args[0]);
+  Local<Function> cb = Local<Function>::Cast(args[1]);
+  const unsigned argc = 1;
+  Local<Value> argv[argc] = { args[0]->ToString() };
+  //Local<Value> argv[argc] = { String::NewFromUtf8(isolate, "hello world") };
+  cb->Call(isolate->GetCurrentContext()->Global(), argc, argv);
+};
+
+
+
+
 void init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "hello", Method);
+  NODE_SET_METHOD(exports, "add", Add);
+  NODE_SET_METHOD(exports, "ord", RunCallback);
 }
 
 NODE_MODULE(addon, init)
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 var addon = require('./build/Release/addon');
 
 console.log(addon.hello()); // 'world'
+
+console.log( 'This should be eight:', addon.add(6,5) );
+
+addon.ord('ssss困困地地要地地要',function(msg){
+  console.log(msg); // 'hello world'
+});
